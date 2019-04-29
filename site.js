0291a30be08ab3af8557230d53ddf5b1e8b4ad13
ste001 <asso99@hotmail.com>
2019-04-29 14:24:43 +0200

    Use Express static files

--- site.js
+++ site.js
@@ -1,21 +1,6 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
+var app = express();
 
-http.createServer(function(req, res) {
-  var q = url.parse(req.url, true);
-  var filename = '.' + q.pathname;
-  if (filename == './'){
-    filename = './index.html'
-  }
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      res.write('<h1>Page not found</h1>')
-      return res.end()
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+app.use(express.static('views'));
+
+app.listen(8080);
